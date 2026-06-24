f94c225e5f81e9b8d1b821e2a86f0378e331ee09
iksanmaulanaa17 <iksanmaulanaa17@gmail.com>
2026-06-24 21:06:26 +0700

    update

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
     <div class="login-container">
         <div class="card card-primary card-outline mb-4">
             <div class="card-header text-center">
-                <h3 class="card-title float-none">Inventory System</h3>
+                <img src="assets/img/gambar 1.jpeg" width="80" height="80" alt="icon">
+                <h3 class="card-title float-none">Fruity Indonesia</h3>
             </div>
             <form action="login.php" method="post">
                 <div class="card-body">
